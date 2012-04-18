07e6ec14f66416c1d81ae1fe542a50c23c03e2c8
Ben Ford <ben.ford@puppetlabs.com>
2012-04-18 09:40:23 -0700

    convert tabs to spaces

--- restapi/main.rb
+++ restapi/main.rb
@@ -5,195 +5,195 @@ require 'restapi.rb'
 
 options = {}
 optparse = OptionParser.new { |opts|
-	opts.banner = "Usage : restapi.rb <commandstring>
+    opts.banner = "Usage : restapi.rb <commandstring>
 
-		Steps for using the API:
-			1:) restapi.rb --init --certname my.cert.name --server my.server.name
-					Builds the config file
-					Generates the certificate and CSR
-					Submits the CSR to the Puppetmaster
+        Steps for using the API:
+            1:) restapi.rb --init --certname my.cert.name --server my.server.name
+                    Builds the config file
+                    Generates the certificate and CSR
+                    Submits the CSR to the Puppetmaster
 
-			2:) On puppetmaster: puppet cert sign my.cert.name
+            2:) On puppetmaster: puppet cert sign my.cert.name
 
-			3:) restapi.rb --install
-					Downloads the signed certificate and installs it
+            3:) restapi.rb --install
+                    Downloads the signed certificate and installs it
 
-			4:) ...
+            4:) ...
 
-			5:) Profit!
+            5:) Profit!
 
-		Your Puppetmaster must be configured to allow requests other than certificate requests.
-		See http://docs.puppetlabs.com/guides/rest_auth_conf.html for more information.
+        Your Puppetmaster must be configured to allow requests other than certificate requests.
+        See http://docs.puppetlabs.com/guides/rest_auth_conf.html for more information.
 
-		The certname can be specified with --certname or with optional CERTNAME argument to many options. 
+        The certname can be specified with --certname or with optional CERTNAME argument to many options. 
 
 "
  
-	options[:show]=false
-	opts.on("-d", "--debug", "runs in debug mode") do |debug|
-		options[:debug] = true
-		#restAPI.debug
-	end
+    options[:show]=false
+    opts.on("-d", "--debug", "runs in debug mode") do |debug|
+        options[:debug] = true
+        #restAPI.debug
+    end
 
-	opts.on("-h", "--help", "Displays this help") do
-		puts opts
-		exit
-	end
+    opts.on("-h", "--help", "Displays this help") do
+        puts opts
+        exit
+    end
 
-	opts.on("--server SERVER", "The server address of your Puppetmaster.") do |server|
-		options[:server] = server
-	end
+    opts.on("--server SERVER", "The server address of your Puppetmaster.") do |server|
+        options[:server] = server
+    end
 
-	opts.on("--certname CERTNAME", "The certname you wish to use when connecting to your Puppetmaster") do |certname|
-		options[:certname] = certname
-	end
+    opts.on("--certname CERTNAME", "The certname you wish to use when connecting to your Puppetmaster") do |certname|
+        options[:certname] = certname
+    end
 
-	opts.on("--file FILENAME", "The filename for use by other options.") do |filename|
-		options[:filename] = filename
-	end
+    opts.on("--file FILENAME", "The filename for use by other options.") do |filename|
+        options[:filename] = filename
+    end
 
-	opts.on("--state STATE", "The desired state you want to set.") do |state|
-		options[:state] = state
-	end
+    opts.on("--state STATE", "The desired state you want to set.") do |state|
+        options[:state] = state
+    end
 
-	opts.separator('')
+    opts.separator('')
 
-	opts.on("--init", "Initialize application. Generate config file, certificate and submit CSR. Requires --certname and --server.") do
-		options[:action] = 'init'
-	end
+    opts.on("--init", "Initialize application. Generate config file, certificate and submit CSR. Requires --certname and --server.") do
+        options[:action] = 'init'
+    end
 
-	opts.on("--install", "Download and install signed certificate.") do
-		options[:action] = 'install'
-	end
+    opts.on("--install", "Download and install signed certificate.") do
+        options[:action] = 'install'
+    end
 
-	opts.separator('')
+    opts.separator('')
 
-	opts.on("--catalog", "Download the catalog compiled for your app's certname. Quite often just the default Node.") do
-		options[:action] = 'catalog'
-	end
+    opts.on("--catalog", "Download the catalog compiled for your app's certname. Quite often just the default Node.") do
+        options[:action] = 'catalog'
+    end
 
-	opts.on("--delete [CERTNAME]", "Remove certificate and facts about a node from Puppetmaster.") do |certname|
-		options[:action] = 'delete'
-		options[:certname] ||= certname
-	end
+    opts.on("--delete [CERTNAME]", "Remove certificate and facts about a node from Puppetmaster.") do |certname|
+        options[:action] = 'delete'
+        options[:certname] ||= certname
+    end
 
-	opts.on("--facts [CERTNAME]", "Retrieve the facts known about a given certname.") do |certname|
-		options[:action] = 'facts'
-		options[:certname] ||= certname
-	end
+    opts.on("--facts [CERTNAME]", "Retrieve the facts known about a given certname.") do |certname|
+        options[:action] = 'facts'
+        options[:certname] ||= certname
+    end
 
-	opts.on("--insert [CERTNAME]", "Send facts for a given certname to the Puppetmaster. Requires --file.") do |certname|
-		options[:action] = 'insert'
-		options[:certname] ||= certname
-	end
+    opts.on("--insert [CERTNAME]", "Send facts for a given certname to the Puppetmaster. Requires --file.") do |certname|
+        options[:action] = 'insert'
+        options[:certname] ||= certname
+    end
 
-	opts.on("--node [CERTNAME]", "Retrieve the node information (including facts) known about a given certname.") do |certname|
-		options[:action] = 'node'
-		options[:certname] ||= certname
-	end
+    opts.on("--node [CERTNAME]", "Retrieve the node information (including facts) known about a given certname.") do |certname|
+        options[:action] = 'node'
+        options[:certname] ||= certname
+    end
 
-	opts.on("--search QUERY", Array, "Retrieve the nodes matching a comma separated query string (e.g. kernel=Linux,virtual=vmware)") do |query|
-		options[:action] = 'search'
-		options[:query]  = query
-	end
+    opts.on("--search QUERY", Array, "Retrieve the nodes matching a comma separated query string (e.g. kernel=Linux,virtual=vmware)") do |query|
+        options[:action] = 'search'
+        options[:query]  = query
+    end
 
-	opts.separator('')
+    opts.separator('')
 
-	opts.on("--certificate [CERTNAME]", "Retrieve the certificate for a given certname or 'ca'.") do |certname|
-		options[:action] = 'certificate'
-		options[:certname] ||= certname
-	end
+    opts.on("--certificate [CERTNAME]", "Retrieve the certificate for a given certname or 'ca'.") do |certname|
+        options[:action] = 'certificate'
+        options[:certname] ||= certname
+    end
 
-	opts.on("--cert_status [CERTNAME]", "Retrieve the certificate status for a given certname. Set the status by using --status.") do |certname|
-		options[:action] = 'certificate_status'
-		options[:certname] ||= certname
-	end
+    opts.on("--cert_status [CERTNAME]", "Retrieve the certificate status for a given certname. Set the status by using --status.") do |certname|
+        options[:action] = 'certificate_status'
+        options[:certname] ||= certname
+    end
 
-	opts.on("--cert_revocation_list", "Retrieve and display the certifiacte revocation list from the master.") do
-		options[:action] = 'certificate_revocation_list'
-	end
+    opts.on("--cert_revocation_list", "Retrieve and display the certifiacte revocation list from the master.") do
+        options[:action] = 'certificate_revocation_list'
+    end
 
-	opts.on("--sign [CERTNAME]", "Instruct the Puppetmaster to sign a certificate. Requires significate privileges in auth.conf.") do |certname|
-		options[:action] = 'sign'
-		options[:certname] ||= certname
-	end
+    opts.on("--sign [CERTNAME]", "Instruct the Puppetmaster to sign a certificate. Requires significate privileges in auth.conf.") do |certname|
+        options[:action] = 'sign'
+        options[:certname] ||= certname
+    end
 
-	opts.separator('')
+    opts.separator('')
 
-	opts.on("--file_metadata PATH", "Retrieve the metadata for a file.") do |path|
-		options[:action] = 'file_metadata'
-		options[:argument] = path
-	end
+    opts.on("--file_metadata PATH", "Retrieve the metadata for a file.") do |path|
+        options[:action] = 'file_metadata'
+        options[:argument] = path
+    end
 
-	opts.on("--getfile PATH", "Download a file from the Puppetmaster. Save to --file or output on stdout.") do |path|
-		options[:action] = 'getfile'
-		options[:argument] = path
-	end
+    opts.on("--getfile PATH", "Download a file from the Puppetmaster. Save to --file or output on stdout.") do |path|
+        options[:action] = 'getfile'
+        options[:argument] = path
+    end
 
-	opts.separator('')
+    opts.separator('')
 
-	opts.on("--resource RESOURCE", "Returns a list of resources (e.g. user) or information about a resource (e.g. 'user/elvis')") do |resource|
-		options[:action] = 'resource'
-		options[:argument] = resource
-	end
+    opts.on("--resource RESOURCE", "Returns a list of resources (e.g. user) or information about a resource (e.g. 'user/elvis')") do |resource|
+        options[:action] = 'resource'
+        options[:argument] = resource
+    end
 
-	opts.on("--report [CERTNAME]", "Sends a YAML report to the Puppetmaster. Requires --file.") do |certname|
-		options[:action] = 'report'
-		options[:certname] ||= certname
-	end
+    opts.on("--report [CERTNAME]", "Sends a YAML report to the Puppetmaster. Requires --file.") do |certname|
+        options[:action] = 'report'
+        options[:certname] ||= certname
+    end
 
-	opts.on("--status", "Check to make sure the Puppetmaster is alive and well.") do
-		options[:action] = 'status'
-	end
+    opts.on("--status", "Check to make sure the Puppetmaster is alive and well.") do
+        options[:action] = 'status'
+    end
 }
 
 begin
-	optparse.parse!
+    optparse.parse!
 
-	restAPI = RestAPI.new(options[:debug])
-	# if certname isn't specified, let's default to our certname, except for init
-	if options[:action] != 'init'
-		options[:certname] ||= restAPI.certname
-	end
+    restAPI = RestAPI.new(options[:debug])
+    # if certname isn't specified, let's default to our certname, except for init
+    if options[:action] != 'init'
+        options[:certname] ||= restAPI.certname
+    end
 
-	case options[:action]
-		when 'init'
-			restAPI.init(options[:certname], options[:server])
-		when 'install'
-			restAPI.install
-		when 'catalog'
-			restAPI.catalog
-		when 'status'
-			restAPI.status
-		when 'facts'
-			restAPI.facts(options[:certname])
-		when 'node'
-			restAPI.node(options[:certname])
-		when 'search'
-			restAPI.search(options[:query])
-		when 'insert'
-			restAPI.insert(options[:certname], options[:filename])
-		when 'delete'
-			restAPI.delete(options[:certname])
-		when 'file_metadata'
-			restAPI.file_metadata(options[:argument])			
-		when 'getfile'
-			restAPI.getfile(options[:argument], options[:filename])
-		when 'certificate'
-			restAPI.certificate(options[:certname])
-		when 'sign'
-			restAPI.sign(options[:certname])
-		when 'certificate_status'
-			restAPI.certificate_status(options[:certname], options[:state])
-		when 'certificate_revocation_list'
-			restAPI.certificate_revocation_list
-		when 'resource'
-			restAPI.resource(options[:argument])
-		when 'report'
-			restAPI.report(options[:certname], options[:filename])
-		else
-			puts 'Use -h/--help for usage documentation.'
-	end		
+    case options[:action]
+        when 'init'
+            restAPI.init(options[:certname], options[:server])
+        when 'install'
+            restAPI.install
+        when 'catalog'
+            restAPI.catalog
+        when 'status'
+            restAPI.status
+        when 'facts'
+            restAPI.facts(options[:certname])
+        when 'node'
+            restAPI.node(options[:certname])
+        when 'search'
+            restAPI.search(options[:query])
+        when 'insert'
+            restAPI.insert(options[:certname], options[:filename])
+        when 'delete'
+            restAPI.delete(options[:certname])
+        when 'file_metadata'
+            restAPI.file_metadata(options[:argument])           
+        when 'getfile'
+            restAPI.getfile(options[:argument], options[:filename])
+        when 'certificate'
+            restAPI.certificate(options[:certname])
+        when 'sign'
+            restAPI.sign(options[:certname])
+        when 'certificate_status'
+            restAPI.certificate_status(options[:certname], options[:state])
+        when 'certificate_revocation_list'
+            restAPI.certificate_revocation_list
+        when 'resource'
+            restAPI.resource(options[:argument])
+        when 'report'
+            restAPI.report(options[:certname], options[:filename])
+        else
+            puts 'Use -h/--help for usage documentation.'
+    end     
 rescue Exception => e
-	puts e
+    puts e
 end
--- restapi/restapi.rb
+++ restapi/restapi.rb
@@ -2,239 +2,239 @@
 require 'yaml'
 
 class RestAPI
-	attr_accessor :server, :certname
+    attr_accessor :server, :certname
 
-	def initialize(dbg = false)
-		@debug = dbg
-		@configdir = File.expand_path('~/.restapi')
+    def initialize(dbg = false)
+        @debug = dbg
+        @configdir = File.expand_path('~/.restapi')
 
-		begin
-			config = YAML.load_file("#{@configdir}/config.yaml")
-			@server   = config['server']
-			@certname = config['certname']
-		rescue Exception => e
-			puts 'Initializing API...'
-		end
-	end
+        begin
+            config = YAML.load_file("#{@configdir}/config.yaml")
+            @server   = config['server']
+            @certname = config['certname']
+        rescue Exception => e
+            puts 'Initializing API...'
+        end
+    end
 
-	#def command(action, argument='', method='GET', type='yaml', output=false, file=false, data=false)
-	def command( opts={} )
-		opts[:method] ||= 'GET'
-		opts[:type] ||= 'yaml'
+    #def command(action, argument='', method='GET', type='yaml', output=false, file=false, data=false)
+    def command( opts={} )
+        opts[:method] ||= 'GET'
+        opts[:type] ||= 'yaml'
 
-		if opts[:noauth]
-			auth = '-k '
-		else
-			auth = "--cert #{@configdir}/#{@certname}.pem --key #{@configdir}/#{@certname}.key --cacert #{@configdir}/ca_crt.pem"
-		end
+        if opts[:noauth]
+            auth = '-k '
+        else
+            auth = "--cert #{@configdir}/#{@certname}.pem --key #{@configdir}/#{@certname}.key --cacert #{@configdir}/ca_crt.pem"
+        end
 
-		output = opts[:output] ? "-o #{opts[:output]}" : ''
-		header = "-H 'Accept: #{opts[:type]}'"
-		
-		case opts[:method]
-			when 'GET'
-				methodstr = ''
-			when 'PUT'
-				methodstr = '-X PUT'
-				header = "-H 'Content-Type: text/#{opts[:type]}'"
+        output = opts[:output] ? "-o #{opts[:output]}" : ''
+        header = "-H 'Accept: #{opts[:type]}'"
+        
+        case opts[:method]
+            when 'GET'
+                methodstr = ''
+            when 'PUT'
+                methodstr = '-X PUT'
+                header = "-H 'Content-Type: text/#{opts[:type]}'"
 
-				if opts[:file]
-					filestr   = "--data-binary @#{opts[:file]}"
-				end
+                if opts[:file]
+                    filestr   = "--data-binary @#{opts[:file]}"
+                end
 
-				if opts[:data]
-					datastr   = "--data '#{opts[:data]}'"
-				end
+                if opts[:data]
+                    datastr   = "--data '#{opts[:data]}'"
+                end
 
-			when 'DELETE'
-				methodstr = '-X DELETE'
-			when 'HEAD'
-				methodstr = '-I'
-		end
+            when 'DELETE'
+                methodstr = '-X DELETE'
+            when 'HEAD'
+                methodstr = '-I'
+        end
 
-		uri = "https://#{@server}:8140/production/#{opts[:action]}/#{opts[:argument]}"
-		cmd = "curl #{auth} #{methodstr} #{output} #{filestr} #{datastr} #{header} \"#{uri}\"" #quoted uri for fact ampersands
-		if @debug
-			puts cmd
-		else
-			if not system(cmd)
-				raise StandardError, 'cURL execution failed.'
-			end
-			puts # newline after curl output
-		end
-	end
+        uri = "https://#{@server}:8140/production/#{opts[:action]}/#{opts[:argument]}"
+        cmd = "curl #{auth} #{methodstr} #{output} #{filestr} #{datastr} #{header} \"#{uri}\"" #quoted uri for fact ampersands
+        if @debug
+            puts cmd
+        else
+            if not system(cmd)
+                raise StandardError, 'cURL execution failed.'
+            end
+            puts # newline after curl output
+        end
+    end
 
-	def init(certname, server)
-		if certname == nil || server == nil
-			puts "Must set server and certname to initialize API"
-			exit
-		end
+    def init(certname, server)
+        if certname == nil || server == nil
+            puts "Must set server and certname to initialize API"
+            exit
+        end
 
-		@certname = certname
-		@server = server
+        @certname = certname
+        @server = server
 
-		if File::directory?( @configdir )
-			require 'fileutils'
-			FileUtils.rm_rf( @configdir )
-		end
+        if File::directory?( @configdir )
+            require 'fileutils'
+            FileUtils.rm_rf( @configdir )
+        end
 
-		Dir.mkdir( @configdir )
-		configfile = File.new("#{@configdir}/config.yaml", 'w')
-			configfile.syswrite("version: 0.1\n")
-			configfile.syswrite("certname: #{@certname}\n")
-			configfile.syswrite("server: #{@server}\n")
-		configfile.close
+        Dir.mkdir( @configdir )
+        configfile = File.new("#{@configdir}/config.yaml", 'w')
+            configfile.syswrite("version: 0.1\n")
+            configfile.syswrite("certname: #{@certname}\n")
+            configfile.syswrite("server: #{@server}\n")
+        configfile.close
 
-		begin
-			if not system("openssl genrsa -out #{@configdir}/#{@certname}.key 1024")
-				raise StandardError, 'Certificate generation failed.'
-			end
+        begin
+            if not system("openssl genrsa -out #{@configdir}/#{@certname}.key 1024")
+                raise StandardError, 'Certificate generation failed.'
+            end
 
-			if not system("openssl req -new -key #{@configdir}/#{@certname}.key -subj '/CN=#{@certname}' -out #{@configdir}/#{@certname}.csr")
-				raise StandardError, 'CSR generation failed.'
-			end
+            if not system("openssl req -new -key #{@configdir}/#{@certname}.key -subj '/CN=#{@certname}' -out #{@configdir}/#{@certname}.csr")
+                raise StandardError, 'CSR generation failed.'
+            end
 
-			self.command( { :action => 'certificate_request',
-							:argument => @certname,
-							:file => "#{@configdir}/#{@certname}.csr",
-							:type => 'plain',
-							:method => 'PUT',
-							:noauth => true } )
+            self.command( { :action => 'certificate_request',
+                            :argument => @certname,
+                            :file => "#{@configdir}/#{@certname}.csr",
+                            :type => 'plain',
+                            :method => 'PUT',
+                            :noauth => true } )
 
-			puts "CSR submitted. Now go sign it on the server and rerun this with --install."
-		rescue Exception => e
-			puts "Failure: #{e.message}"
-		end
-	end
+            puts "CSR submitted. Now go sign it on the server and rerun this with --install."
+        rescue Exception => e
+            puts "Failure: #{e.message}"
+        end
+    end
 
-	def install
-		begin
-			self.command( { :action => 'certificate',
-							:argument => @certname,
-							:output => "#{@configdir}/#{@certname}.pem",
-							:type => 's',
-							:noauth => true } )
+    def install
+        begin
+            self.command( { :action => 'certificate',
+                            :argument => @certname,
+                            :output => "#{@configdir}/#{@certname}.pem",
+                            :type => 's',
+                            :noauth => true } )
 
-			self.command( { :action => 'certificate',
-							:argument => 'ca',
-							:output => "#{@configdir}/ca_crt.pem",
-							:type => 's',
-							:noauth => true } )
+            self.command( { :action => 'certificate',
+                            :argument => 'ca',
+                            :output => "#{@configdir}/ca_crt.pem",
+                            :type => 's',
+                            :noauth => true } )
 
-			puts "Certificate installed. API ready for use."
-		rescue Exception => e
-			puts "Failure: #{e.message}"
-		end
-	end
+            puts "Certificate installed. API ready for use."
+        rescue Exception => e
+            puts "Failure: #{e.message}"
+        end
+    end
 
-	def catalog
-		self.command( { :action => 'catalog', 
-						:argument => @certname } )
-	end
+    def catalog
+        self.command( { :action => 'catalog', 
+                        :argument => @certname } )
+    end
 
-	def status
-		self.command( { :action => 'status', 
-						:argument => 'no_key' } )
-	end
+    def status
+        self.command( { :action => 'status', 
+                        :argument => 'no_key' } )
+    end
 
-	def facts(node)
-		self.command( { :action => 'facts', 
-						:argument => node } )
-	end
+    def facts(node)
+        self.command( { :action => 'facts', 
+                        :argument => node } )
+    end
 
-	def search(query)
-		query.map!{ |item| "facts.#{item}"}
-		self.command( { :action => 'facts_search', 
-						:argument => "search?#{query.join('&')}" } )
-	end
+    def search(query)
+        query.map!{ |item| "facts.#{item}"}
+        self.command( { :action => 'facts_search', 
+                        :argument => "search?#{query.join('&')}" } )
+    end
 
-	def node(node)
-		self.command( { :action => 'node', 
-						:argument => node } )
-	end
+    def node(node)
+        self.command( { :action => 'node', 
+                        :argument => node } )
+    end
 
-	def insert(node, path)
-		self.command( { :action => 'facts', 
-						:argument => node, 
-						:method => 'PUT', 
-						:file => path } )
-	end
+    def insert(node, path)
+        self.command( { :action => 'facts', 
+                        :argument => node, 
+                        :method => 'PUT', 
+                        :file => path } )
+    end
 
-	def file_metadata(path)
-		self.command( { :action => 'file_content',
-						:argument => path,
-						:type => 'yaml' } )
-	end
+    def file_metadata(path)
+        self.command( { :action => 'file_content',
+                        :argument => path,
+                        :type => 'yaml' } )
+    end
 
-	def getfile(path, filename=nil)
-		self.command( { :action => 'file_content',
-						:argument => path,
-						:type => 'raw',
-						:output => filename } )
-	end
+    def getfile(path, filename=nil)
+        self.command( { :action => 'file_content',
+                        :argument => path,
+                        :type => 'raw',
+                        :output => filename } )
+    end
 
-	def delete(node)
-		self.certificate_status(node, 'revoked')
-		self.command( { :action => 'certificate_status', 
-						:argument => node, 
-						:method => 'DELETE', 
-						:type => 'pson' } )
-	end
+    def delete(node)
+        self.certificate_status(node, 'revoked')
+        self.command( { :action => 'certificate_status', 
+                        :argument => node, 
+                        :method => 'DELETE', 
+                        :type => 'pson' } )
+    end
 
-	def certificate(node)
-		self.command( { :action => 'certificate', 
-						:argument => node, 
-						:type => 's' } )
-	end
+    def certificate(node)
+        self.command( { :action => 'certificate', 
+                        :argument => node, 
+                        :type => 's' } )
+    end
 
-	def sign(node)
-		self.command( { :action => 'certificate_status', 
-						:argument => node, 
-						:method => 'PUT', 
-						:type => 'pson',
-						:noauth => true, 
-						:data => "{\"desired_state\":\"signed\"}" } )
-	end
+    def sign(node)
+        self.command( { :action => 'certificate_status', 
+                        :argument => node, 
+                        :method => 'PUT', 
+                        :type => 'pson',
+                        :noauth => true, 
+                        :data => "{\"desired_state\":\"signed\"}" } )
+    end
 
-	def certificate_revocation_list
-		self.command( { :action => 'certificate_revocation_list', 
-						:argument => 'ca', 
-						:type => 's' } )
-	end
+    def certificate_revocation_list
+        self.command( { :action => 'certificate_revocation_list', 
+                        :argument => 'ca', 
+                        :type => 's' } )
+    end
 
-	def certificate_status(node, state=nil)
-		if node == nil
-			self.command( { :action => 'certificate_statuses', 
-							:action => 'no_key', 
-							:type => 'pson' } )
-		elsif state == nil
-			self.command( { :action => 'certificate_status', 
-							:argument => node, 
-							:type => 'pson' } )
-		else
-			self.command( { :action => 'certificate_status', 
-							:argument => node, 
-							:method => 'PUT', 
-							:type => 'pson', 
-							:data => "{\"desired_state\":\"#{state}\"}" } )
-		end
-	end
+    def certificate_status(node, state=nil)
+        if node == nil
+            self.command( { :action => 'certificate_statuses', 
+                            :action => 'no_key', 
+                            :type => 'pson' } )
+        elsif state == nil
+            self.command( { :action => 'certificate_status', 
+                            :argument => node, 
+                            :type => 'pson' } )
+        else
+            self.command( { :action => 'certificate_status', 
+                            :argument => node, 
+                            :method => 'PUT', 
+                            :type => 'pson', 
+                            :data => "{\"desired_state\":\"#{state}\"}" } )
+        end
+    end
 
-	def resource(resource)
-		self.command( { :action => 'resource', 
-						:argument => resource } )
-	end
+    def resource(resource)
+        self.command( { :action => 'resource', 
+                        :argument => resource } )
+    end
 
-	def report(node, file)
-		self.command( { :action => 'report', 
-						:argument => node, 
-						:method => 'PUT', 
-						:file => file } )
-	end
+    def report(node, file)
+        self.command( { :action => 'report', 
+                        :argument => node, 
+                        :method => 'PUT', 
+                        :file => file } )
+    end
 
-	def debug
-		puts "@configdir: #{@configdir}"
-		puts "   @server: #{@server}"
-		puts " @certname: #{@certname}"
-	end
+    def debug
+        puts "@configdir: #{@configdir}"
+        puts "   @server: #{@server}"
+        puts " @certname: #{@certname}"
+    end
 end
